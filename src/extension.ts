850af859a5e9b57b5f8ee382074f8f8510ba6dec
trandaison <sontd.it@gmail.com>
2023-10-22 14:54:59 +0700

    Create extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,101 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { parse } from 'path';
+import { stat } from 'fs-extra';
+import { PluginSettings } from './types';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('vscode-duplicate is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-duplicate" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    'duplicate.execute',
+    (uri: vscode.TextDocument | vscode.Uri) => {
+      const settings = vscode.workspace
+        .getConfiguration()
+        .get('duplicate') as PluginSettings;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-duplicate.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-duplicate!');
-	});
+      if (!uri || !(uri as vscode.Uri).fsPath) {
+        const { activeTextEditor } = vscode.window;
+        if (!activeTextEditor) {
+          vscode.window.showErrorMessage('No active text editor');
+          return;
+        }
 
-	context.subscriptions.push(disposable);
+        duplicate(activeTextEditor.document.uri, settings);
+      }
+
+      duplicate(uri as vscode.Uri, settings);
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
+
+async function duplicate(uri: vscode.Uri, settings: PluginSettings) {
+  const { fsPath } = uri;
+  const { ext, name, dir } = parse(fsPath);
+  const fileStats = await stat(fsPath);
+  const isFile = fileStats.isFile();
+  const isDirectory = fileStats.isDirectory();
+  let newName = `${name}${settings.suffix}${settings.keepFileExt ? ext : ''}`;
+
+  let i = 1;
+  while (true) {
+    const fileExists = await exists(newName, dir, isFile);
+    if (fileExists) {
+      newName = `${name}${settings.suffix} (${i})${
+        settings.keepFileExt ? ext : ''
+      }`;
+      i++;
+    } else {
+      break;
+    }
+  }
+  const newPath = `${dir}/${newName}`;
+
+  try {
+    const newUri = vscode.Uri.file(newPath);
+    if (isFile) {
+      await vscode.workspace.fs.copy(uri, vscode.Uri.file(newPath));
+      const newFile = await vscode.workspace.openTextDocument(newUri);
+      await vscode.window.showTextDocument(newFile);
+      await vscode.commands.executeCommand('renameFile');
+    } else if (isDirectory) {
+      const { fsPath } = newUri;
+      const { name } = parse(fsPath);
+      const finalName = await vscode.window.showInputBox({
+        value: name,
+        prompt: 'Enter new folder name',
+        validateInput: async (value) => {
+          if (value === '') {
+            return "Folder name cannot be empty (Press 'Esccape' to cancel)";
+          }
+          const dirExists = await exists(value, dir, false);
+          if (dirExists) {
+            return 'Folder already exists';
+          }
+        },
+      });
+      if (finalName === undefined) {
+        return;
+      }
+
+      const finalPath = `${dir}/${finalName}`;
+      await vscode.workspace.fs.copy(uri, vscode.Uri.file(finalPath));
+    } else {
+      vscode.window.showErrorMessage('Not a file or directory');
+    }
+  } catch (error: any) {
+    vscode.window.showErrorMessage(error.message);
+  }
+}
+
+async function exists(name: string, dir?: string, isFile: boolean = true) {
+  try {
+    const stats = await stat(dir ? `${dir}/${name}` : name);
+    return isFile ? stats.isFile() : stats.isDirectory();
+  } catch (error) {
+    return false;
+  }
+}
---
+++ src/types/index.d.ts
@@ -0,0 +1,5 @@
+export interface PluginSettings {
+  autoOpen: boolean;
+  keepFileExt: boolean;
+  suffix: string;
+}
